c8284f5a6c05f1492bf88f44ddf46d07ac4c4ca0
Julius Stotz <julius.stotz@t-online.de>
2025-12-18 19:13:40 +0100

    Added files for day 4

--- aoc2025_3.cpp
+++ aoc2025_3.cpp
@@ -4,32 +4,29 @@
 #include <sstream>
 #include <vector>
 #include <array>
-#include <ranges>
+#include <cmath>
 
 int main(){
-    std::ifstream file{"aoc2025_2.txt"};
+    std::ifstream file{"aoc2025_3.txt"};
 
     unsigned long long sum = 0;
     std::string line;
     while(std::getline(file, line)){
-        // Find largest two digits with their positions
-        unsigned int max = 0;
+        // Find largest twelve digits with their positions
         int pos = -1;
-        for(auto i = 0; i < line.size() - 1; ++i){
-            const unsigned int val = line[i] - '0';
-            if(val > max){
-                max = val;
-                pos = i;
+        unsigned int max = 0;
+        for(int i = 11 ; i >= 0; --i){
+            for(auto j = pos + 1; j < line.size() - i; ++j){
+                const unsigned int val = line[j] - '0';
+                if(val > max){
+                    max = val;
+                    pos = j;
+                }
             }
+            std::cout << "Found max " << max << " at pos " << pos << std::endl;
+            sum += max * std::pow(10, i);
+            max = 0;
         }
-        sum = max * 10;
-        max = 0;
-        for(auto i = pos + 1; i < line.size(); ++i){
-            const unsigned int val = line[i] - '0';
-            if(val > max)
-                max = val;
-        }
-        sum += max;
     }
     std::cout << sum << std::endl;
 }
