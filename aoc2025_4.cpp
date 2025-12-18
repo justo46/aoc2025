33bcf6073004ab0a6b0dbc943f1dbc9b3fa5012c
Julius Stotz <julius.stotz@t-online.de>
2025-12-19 00:24:51 +0100

    Structure of day 4 done

--- aoc2025_4.cpp
+++ aoc2025_4.cpp
@@ -8,17 +8,29 @@
 
 bool check_paper(const std::vector<std::string>& grid, int row, int col){
     // Return true if there are 4 or less '@' in the adjacent cells
+    const int rows = grid.size();
+    const int cols = grid[0].size();
+    int count = 0;
+
 }
 
 int main(){
     std::ifstream file{"aoc2025_3.txt"};
 
-    unsigned long long sum = 0;
     std::vector<std::string>grid;
     std::string line;
     while(std::getline(file, line)){
         grid.push_back(line);
     }
-
+    unsigned long long sum = 0;
+    const int rows = grid.size();
+    const int cols = grid[0].size();
+    for(int row = 0; row < rows; ++row){
+        for(int col = 0; col < cols; ++col){
+            if(grid[row][col] == '@'){
+                sum += check_paper(grid, row, col);
+            }
+        }
+    }
     std::cout << sum << std::endl;
 }
