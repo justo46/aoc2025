a79b5740cc35e380ec25e39de55b20e767e6a93b
Julius Stotz <julius.stotz@t-online.de>
2025-12-30 01:00:21 +0100

    Solved day 6 part 1

--- aoc2025_6.cpp
+++ aoc2025_6.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -14,16 +13,23 @@ int main(){
     std::ifstream file{"aoc2025_6.txt"};
 
     std::vector<std::vector<uint64_t>> numbers;
-
     std::string line;
-    while(std::getline(file, line) && !line.empty()){
+    while(std::getline(file, line)){
+        if (line[0] == '*' || line[0] == '+') break;
         std::vector<uint64_t> row;
         for (auto segment : std::views::split(line, ' ')){
-            auto num = std::stoull(std::string(segment.begin(), segment.end()));
-            row.push_back(num);
+            if (segment.empty()) continue;
+            row.push_back(std::stoull(std::ranges::to<std::string>(segment)));
         }
         numbers.push_back(row);
     }
+    
+    std::vector<char> operators;
+    for (char c : line){
+        if (c == '+' || c == '*')
+            operators.push_back(c);
+    }
+
     // Transpose (Could read input differently to avoid this step)
     std::vector<std::vector<uint64_t>> transposed(numbers[0].size(), std::vector<uint64_t>(numbers.size()));
     for (size_t i = 0; i < numbers.size(); ++i){
@@ -31,15 +37,16 @@ int main(){
             transposed[j][i] = numbers[i][j];
         }
     }
-    
-    int sum = 0;
+
+    uint64_t sum = 0;
     // Depending on the last character of the vector (+ or *), perform the corresponding operation using std::reduce
-    for(const auto& vec : transposed){
-        auto op = static_cast<char>(vec.back());
+    for(size_t i = 0; i < transposed.size(); ++i){
+        const auto& vec = transposed[i];
+        const auto& op = operators[i];
         if (op == '+'){
-            sum += std::reduce(vec.begin(), vec.end() - 1, 0ULL, std::plus<>());
+            sum += std::reduce(vec.begin(), vec.end(), 0ULL, std::plus<>());
         } else if (op == '*'){
-            sum += std::reduce(vec.begin(), vec.end() - 1, 1ULL, std::multiplies<>());
+            sum += std::reduce(vec.begin(), vec.end(), 1ULL, std::multiplies<>());
         }
     }
     std::cout << sum;
